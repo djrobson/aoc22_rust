fa45faffa4cb6de77b62b252c3afb10ddc6ecfed
djrobson <djrobson@users.noreply.github.com>
2022-12-06 14:17:33 -0500

    day06 fixed comments

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -7,22 +7,22 @@ fn main() -> io::Result<()> {
     // Open the file in read-only mode
     let mut file = fs::File::open("input06.txt")?;
 
-    // Create a new string to store the contents of the file
+    // Create a new vec to store the contents of the file
     let mut contents: Vec<u8> = Vec::new();
 
-    // Read the entire file into the string
+    // Read the entire file into the vec
     file.read_to_end(&mut contents)?;
 
+    // preload the counter with the length of the window
     let mut count = 13;
 
-    // Print each line
+    // iterate through the vec in groups of 14
     for seq in contents.windows(14) {
         count += 1;
         let h: HashSet<&u8> = HashSet::from_iter(seq);
-        if h.len() == 14 {
+        if h.len() == 14 { // did we find 14 unique entries?
                 println!("found new seq {:?} at {}", seq, count);
                 break;
-
         }
     }
 
