b8048350d8b04997a7f9f9e5cd64b16288a3b658
djrobson <djrobson@users.noreply.github.com>
2022-12-10 20:46:22 -0500

    failed to solve day08, maybe some off-by-one on edge counting?

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -1,130 +1,83 @@
+use std::io;
 
-fn is_visible(x: usize, y: usize, grid: &Vec<Vec<u8>>) -> bool {
-    let max = grid.len();
-    let my_height = grid[y][x];
-    let mut block_left = false;
-    let mut block_right = false;
-    let mut block_up = false;
-    let mut block_down = false;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let input: Vec<String> = io::stdin().lines().flatten().collect();
 
-    if x == 0 || x == max || y == 0 || y == max {
+    let grid: Vec<Vec<u32>> =
+        input.iter().map(|s| s.chars().flat_map(|c| c.to_digit(10)).collect()).collect();
+    //println!("{:#?}", grid);
+
+    let rows = grid.len();
+    let cols = grid[0].len();
+
+    let mut total = (rows * 2) + ((cols - 2) * 2);
+    for row in 1..rows - 1 {
+        for col in 1..cols - 1 {
+            if is_visible(&grid, row, col) {
+                total += 1;
+            }
+        }
+    }
+    println!("{}", total);
+
+    let mut best = 0;
+    for row in 0..rows {
+        for col in 0..cols {
+            best = get_score(&grid, row, col).max(best);
+        }
+    }
+    println!("{}", best);
+
+    Ok(())
+}
+
+fn grid_col<T: Copy>(grid: &[Vec<T>], col: usize) -> Vec<T> {
+    grid.iter().map(|v| v[col]).collect()
+}
+
+fn is_visible(grid: &[Vec<u32>], row: usize, col: usize) -> bool {
+    let value = &grid[row][col];
+
+    let r = &grid[row];
+    if r[0..col].iter().all(|x| x < value) || r[col + 1..].iter().all(|x| x < value) {
         return true;
     }
 
-    for left in 0..x {
-        if grid[y][left] >= my_height {
-            block_left = true;
-            break;
-        }
-    }
-    for right in x+1..max {
-        if grid[y][right] >= my_height {
-            block_right = true;
-            break;
-        }
-    }
-    for up in 0..y {
-        if grid[up][x] >= my_height {
-            block_up = true;
-            break;
-        }
-    }
-    for down in y+1..max {
-        if grid[down][x] >= my_height {
-            block_down = true;
-            break;
-        }
+    let c = grid_col(grid, col);
+    if c[0..row].iter().all(|x| x < value) || c[row + 1..].iter().all(|x| x < value) {
+        return true;
     }
 
-    !(block_left && block_right && block_up && block_down)
-}
-fn get_visible(x: usize, y: usize, grid: &Vec<Vec<u8>>) -> usize {
-    let max = grid.len();
-    let my_height = grid[y][x];
-    let mut count_left = 0;
-    let mut count_right = 0;
-    let mut count_up = 0;
-    let mut count_down = 0;
-
-    if x==0 || x== max || y == 0 || y == max {
-        return 0;
-    }
-
-    let mut left = x;
-    while left > 0
-    {
-        count_left = count_left +1;
-        if grid[y][left-1] >= my_height {
-            break;
-        }
-        left = left -1;
-    }
-    let mut right = x+1;
-    while right < max
-    {
-        count_right = count_right +1;
-        if grid[y][right] >= my_height {
-            break;
-        }
-        right = right +1;
-    }
-    
-    let mut up = y;
-    while up > 0
-    {
-        count_up = count_up +1;
-        if grid[up-1][x] >= my_height {
-            break;
-        }
-        up = up -1;
-    }
-
-    let mut down = y+1;
-    while down < max
-    {
-        count_down = count_down +1;
-        if grid[down][x] < my_height {
-            break;
-        }
-        down = down +1;
-    }
-
-    return count_down * count_up * count_left * count_right;
+    false
 }
 
-fn main() {
-    //let f = include_str!("../sample.txt");
-    let f = include_str!("../input08.txt");
-    let mut grid: Vec<Vec<u8>> = Vec::new();
+fn get_score(grid: &[Vec<u32>], row: usize, col: usize) -> usize {
+    let value = &grid[row][col];
+    let mut score = 1;
 
-    for line in f.lines() {
-        grid.push(line.as_bytes().to_vec());
+    let r = &grid[row];
+    let mut trees = r[0..col].iter().rev().take_while(|&x| x < value).count();
+    if trees < r[0..col].len() {
+        trees += 1;
     }
-    //println!("{:?}", grid);
-
-    let mut count = 0;
-    //let edge = grid.len();
-
-    for y in 0..grid.len() {
-        for x in 0..grid[0].len() {
-            if is_visible(x, y, &grid) {
-                count = count +1;
-            }
-        }
+    score *= trees;
+    let mut trees = r[col + 1..].iter().take_while(|&x| x < value).count();
+    if trees < r[col + 1..].len() {
+        trees += 1;
     }
-    println!("outside visible trees {}", count);
+    score *= trees;
 
-    let mut max_v = 0;
-    for y in 0..grid.len() {
-        for x in 0..grid[0].len() {
-            let v =  get_visible(x, y, &grid);
-            //println!("{} {} {}", x, y, v);
-            if v > max_v
-            {
-                max_v = v;
-            }
-        }
+    let c = grid_col(grid, col);
+    let mut trees = c[0..row].iter().rev().take_while(|&x| x < value).count();
+    if trees < c[0..row].len() {
+        trees += 1;
     }
-    println!("inside visible trees {}", max_v);
+    score *= trees;
+    let mut trees = c[row + 1..].iter().take_while(|&x| x < value).count();
+    if trees < c[row + 1..].len() {
+        trees += 1;
+    }
+    score *= trees;
 
+    score
 }
