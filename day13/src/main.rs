1248e5adcbec9435d977be0dfdc28103c310e318
djrobson <djrobson@users.noreply.github.com>
2022-12-14 10:28:40 -0500

    day13 step 1 solved

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -2,15 +2,13 @@ use nom::{
     branch::alt,
     bytes::complete::tag,
     multi::{separated_list0,separated_list1},
-    character::complete::newline,
     sequence::{delimited,separated_pair},
     IResult,
     Parser,
 };
 
 use std::{
-    //cmp::Ordering::{self, *},
-    fmt::Display,
+    cmp::Ordering::{self, *},
 };
 
 #[derive(Debug)]
@@ -19,26 +17,41 @@ enum Value {
     List(Vec<Value>),
 }
 
-/*
-impl Display for Value {
-    fn fmt( &self, f: &mut std::fmt::Formatter<'_>,
-    ) -> std::fmt::Result {
-        write!(
-            f,
-            "{}",
-            match self {
-                Value::List(list) => format!(
-                    "[{}]",
-                    list.iter()
-                        .map(|v| v.to_string())
-                        .intersperse(",".to_string())
-                        .collect::<String>()
-                ),
-                Value::Val(num) => num.to_string(),
-            },
-        )
+impl Eq for Value {
+
+}
+
+impl PartialEq for Value {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Self::List(l0), Self::List(r0)) => l0 == r0,
+            (Self::Val(l0), Self::Val(r0))  => l0 == r0,
+            (Self::List(l0), Self::Val(r0)) => {
+                l0 == &vec![Value::Val(*r0)]
+            }
+            (Self::Val(l0), Self::List(r0)) => {
+                &vec![Value::Val(*l0)] == r0
+            }
+        }
     }
-}*/
+}
+
+impl PartialOrd for Value {
+    fn partial_cmp(&self, other:&Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Value {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match (self, other) {
+            (Value::List(a), Value::List(b)) => a.cmp(b),
+            (Value::List(a), Value::Val(b)) => a.cmp(&vec![Value::Val(*b)]),
+            (Value::Val(a),Value::List(b)) => vec![Value::Val(*a)].cmp(&b),
+            (Value::Val(a), Value::Val(b)) => a.cmp(b)
+        }
+    }
+}
 
 fn parse_packet(input: &str) -> IResult<&str, Value> {
     alt((
@@ -63,7 +76,7 @@ fn parse_input(input: &str) ->IResult<&str, Vec<(Value,Value)>>
 }
 
 fn main() {
-    const IS_SAMPLE: bool = true;
+    const IS_SAMPLE: bool = false;
     const INPUT: &str = if IS_SAMPLE {
         include_str!("../sample.txt")
     } else {
@@ -71,5 +84,13 @@ fn main() {
     };
 
     let input = parse_input(INPUT).unwrap().1;
-    println!("stuff here {:?}", input);
-}
+    let mut count = 1;
+    let mut total = 0;
+    for pair in input {
+        let (left, right) = pair;
+        if left < right {
+            total += count;
+        }
+        count += 1;
+    }
+    println!("step 1 {total}");}
