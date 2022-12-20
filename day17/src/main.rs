3cdf38ca06fa9ef4069728a4d5a1948c3820bb64
djrobson <djrobson@users.noreply.github.com>
2022-12-20 13:47:38 -0500

    day17 step 1 lots of progress

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -1,5 +1,5 @@
 const IS_SAMPLE: bool = true;
-const WIDTH: usize = 8;
+const WIDTH: usize = 7;
 
 #[derive(Clone, Copy, PartialEq, Eq)]
 enum Direction {
@@ -31,6 +31,21 @@ fn print_chamber( chamber: & Vec<Vec<u8>>) {
     print_chamber_range(chamber, chamber.len()-1, 0);
 }
 
+fn set_shape_at_coord( chamber: &mut Vec<Vec<u8>>, shape: &Vec<Vec<u8>>, coords: (i32,i32), new_val: u8) -> () {
+    let shape_height = shape.len();
+    let shape_width = shape[0].len();
+    for sy in 0..shape_height { // for each verticle row in the shape
+        for sx in 0..shape_width {
+            if shape[sy][sx] == 1 { // check the shape bitmap
+                let x = (coords.0) as usize + sx;
+                let y = coords.1 as usize - sy;
+                println!("chamber at x={x} y={y} was {} now {}", chamber[y][x], new_val );
+                chamber[y][x] = new_val;
+            }
+        }
+    }
+}
+
 fn attempt_shift( chamber: &mut Vec<Vec<u8>>, direction: Direction, shape: &Vec<Vec<u8>>, coords: &mut (i32,i32)) -> bool {
     let shape_height = shape.len();
     let shape_width = shape[0].len();
@@ -44,54 +59,60 @@ fn attempt_shift( chamber: &mut Vec<Vec<u8>>, direction: Direction, shape: &Vec<
             for sy in 0..shape_height { // for each verticle row in the shape
                 for sx in 0..shape_width {
                     if shape[sy][sx] == 1 {
-                        if chamber[coords.1 as usize][(coords.0-1) as usize] == 2 {
+                        if chamber[coords.1 as usize -sy][(coords.0-1) as usize + sx] == 2 {
                             return false;
                         }
                     }
                 }
             }
+            set_shape_at_coord(chamber, shape, *coords, 0);
             *coords = (coords.0 -1, coords.1);
+            set_shape_at_coord(chamber, shape, *coords, 1);
         },
         Direction::Right => {
             // if we're on the right wall then fail
-            if coords.0 as usize + shape.len() >= WIDTH {
+            if coords.0 as usize + shape_width >= WIDTH {
                 return false;
             }
             // check for collision
             for sy in 0..shape_height { // for each verticle row in the shape
                 for sx in 0..shape_width {
                     if shape[sy][sx] == 1 {
-                        print_chamber(&chamber);
-                        if chamber[coords.1 as usize][(coords.0+1) as usize] == 2 {
+                        if chamber[coords.1 as usize - sy][(coords.0+1) as usize + sx] == 2 {
                             return false;
                         }
                     }
                 }
             }
+            set_shape_at_coord(chamber, shape, *coords, 0);
             *coords = (coords.0 +1, coords.1);
+            set_shape_at_coord(chamber, shape, *coords, 1);
         },
         Direction::Down => {
             // if we're on the bottom then fail
-            if coords.1 == 0 {
+            if coords.1 - shape_height as i32 <= 0 {
+                set_shape_at_coord(chamber, shape, *coords, 2);
                 return false;
             }
             // check for collision
             for sy in 0..shape_height { // for each verticle row in the shape
                 for sx in 0..shape_width {
                     if shape[sy][sx] == 1 {
-                        if chamber[(coords.1 -1) as usize][(coords.0) as usize] == 2 {
+                        if chamber[(coords.1 -1) as usize - sy][(coords.0) as usize + sx] == 2 {
                             return false;
                         }
                     }
                 }
             }
+            set_shape_at_coord(chamber, shape, *coords, 0);
             *coords = (coords.0, coords.1 -1);
+            set_shape_at_coord(chamber, shape, *coords, 1);
         },
     }
     true
 }
 fn main() {
-    let INPUT: Vec<Direction> = if IS_SAMPLE {
+    let input: Vec<Direction> = if IS_SAMPLE {
             b">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"
         } else {
             include_str!("../input17.txt").as_bytes()
@@ -131,31 +152,37 @@ fn main() {
 
         // add 3 rows
         let mut empty_rows: usize = 0;
-        loop {
-            if let Some(row) = chamber.get(chamber.len() - empty_rows) {
-                if row.iter().all(|s| *s ==  0) {
-                    empty_rows += 1;
-                } else {
-                    break;
-                }
-            } else { // we hit the bottom
+        for row in chamber.iter().rev() {
+            if row.iter().all(|s| *s ==  0) {
+                empty_rows += 1;
+            } else {
                 break;
             }
         }
+
         for _r in empty_rows..VSPACE {
             chamber.push(vec![0;WIDTH]);
         }
 
         // add next rock
         let rock_idx = rock_count % shapes.len();
-        let mut shape_top_left = (2, chamber.len() as i32 + shapes[rock_idx].len() as i32);
+        let mut shape_top_left = (2, (chamber.len() -1) as i32 + (shapes[rock_idx].len() -1) as i32);
         for line in shapes[rock_idx].iter().rev() { // consider pre-flipping?
-            chamber.push(line.clone());
+            let mut padded_line: Vec<u8> = Vec::new();
+            padded_line.push(0);
+            padded_line.push(0);
+            padded_line.append(&mut line.clone());
+            while padded_line.len() < WIDTH {
+                padded_line.push(0);
+            }
+            chamber.push(padded_line);
         }
 
         'rock_movement: loop {
+    
+            print_chamber(&chamber);
             // puff of air
-            attempt_shift(&mut chamber, INPUT[wind_index], &shapes[rock_idx], &mut shape_top_left);
+            attempt_shift(&mut chamber, input[wind_index], &shapes[rock_idx], &mut shape_top_left);
             wind_index += 1;
             // decend one
             if !attempt_shift(&mut chamber, Direction::Down, &shapes[rock_idx], &mut shape_top_left) {// down
