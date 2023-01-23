4870ffe528664020adfb6562a5d2a013c80136cc
Dale Robson <drobson@gmail.com>
2023-01-22 20:54:13 -0500

    day22 step1 done

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -1,4 +1,6 @@
-const IS_SAMPLE: bool = true;
+use std::fmt;
+
+const IS_SAMPLE: bool = false;
 const PHASE: u8 = 1;
 
 struct Input {
@@ -14,78 +16,94 @@ enum GridVal {
     Rock,
     Void,
 }
-#[derive(Clone,Copy)]
-struct GridLocation{
+#[derive(Clone, Copy)]
+struct GridLocation {
     val: GridVal,
     orig_row: u32,
-    orig_cal: u32,
+    orig_col: u32,
 }
 
-#[derive(Copy,Clone)]
+#[derive(Copy, Clone)]
 enum Facing {
     Top,
     Left,
     Right,
     Bottom,
 }
+impl fmt::Debug for Facing {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Top => write!(f, "^"),
+            Left => write!(f, "<"),
+            Right => write!(f, ">"),
+            Bottom => write!(f, "v"),
+        }
+    }
+}
 use Facing::*;
 
-fn grid_order() -> Vec<Vec<(Facing, usize)>>
-{
+fn grid_order() -> Vec<Vec<(Facing, usize)>> {
     // consider auto discoverying the order from the input
     let result = match (PHASE, IS_SAMPLE) {
-        (1, true) =>
-            vec![
-                vec![(Bottom, 6), (Right, 1), (Left, 1), (Top, 2)],
-                vec![(Bottom, 1), (Right, 4), (Left, 5), (Top, 6)],
-                vec![(Bottom, 3), (Right, 6), (Left, 6), (Top, 3)],
-                vec![(Bottom, 4), (Right, 5), (Left, 2), (Top, 4)],
-                vec![(Bottom, 5), (Right, 2), (Left, 4), (Top, 5)],
-                vec![(Bottom, 2), (Right, 3), (Left, 2), (Top, 1)],
-            ],
-        (1, false) => 
-            vec![
-                vec![(Bottom, 6), (Right, 3), (Left, 3), (Top, 2)],
-                vec![(Bottom, 1), (Right, 2), (Left, 2), (Top, 6)],
-                vec![(Bottom, 3), (Right, 1), (Left, 1), (Top, 3)],
-                vec![(Bottom, 5), (Right, 6), (Left, 6), (Top, 5)],
-                vec![(Bottom, 4), (Right, 5), (Left, 5), (Top, 4)],
-                vec![(Bottom, 1), (Right, 4), (Left, 4), (Top, 1)],
-            ],
-        (2, _) => 
-            vec![
-                vec![(Top, 5), (Left, 4), (Bottom, 3), (Right, 2)],
-                vec![(Top, 4), (Left, 6), (Bottom, 1), (Right, 3)],
-                vec![(Top, 6), (Left, 5), (Bottom, 2), (Right, 1)],
-                vec![(Top, 2), (Left, 1), (Bottom, 6), (Right, 5)],
-                vec![(Top, 1), (Left, 3), (Bottom, 4), (Right, 6)],
-                vec![(Top, 3), (Left, 2), (Bottom, 5), (Right, 4)],
-            ],
+        (1, true) => vec![
+            vec![(Bottom,0);4],
+            vec![(Bottom, 6), (Right, 1), (Left, 1), (Top, 2)],
+            vec![(Bottom, 1), (Right, 4), (Left, 5), (Top, 6)],
+            vec![(Bottom, 3), (Right, 6), (Left, 6), (Top, 3)],
+            vec![(Bottom, 4), (Right, 5), (Left, 2), (Top, 4)],
+            vec![(Bottom, 5), (Right, 2), (Left, 4), (Top, 5)],
+            vec![(Bottom, 2), (Right, 3), (Left, 2), (Top, 1)],
+        ],
+        (1, false) => vec![
+            vec![(Bottom,0);4],
+            vec![(Bottom, 6), (Right, 3), (Left, 3), (Top, 2)],
+            vec![(Bottom, 1), (Right, 2), (Left, 2), (Top, 6)],
+            vec![(Bottom, 3), (Right, 1), (Left, 1), (Top, 3)],
+            vec![(Bottom, 5), (Right, 6), (Left, 6), (Top, 5)],
+            vec![(Bottom, 4), (Right, 5), (Left, 5), (Top, 4)],
+            vec![(Bottom, 1), (Right, 4), (Left, 4), (Top, 1)],
+        ],
+        (2, _) => vec![
+            vec![(Bottom,0);4],
+            vec![(Top, 5), (Left, 4), (Bottom, 3), (Right, 2)],
+            vec![(Top, 4), (Left, 6), (Bottom, 1), (Right, 3)],
+            vec![(Top, 6), (Left, 5), (Bottom, 2), (Right, 1)],
+            vec![(Top, 2), (Left, 1), (Bottom, 6), (Right, 5)],
+            vec![(Top, 1), (Left, 3), (Bottom, 4), (Right, 6)],
+            vec![(Top, 3), (Left, 2), (Bottom, 5), (Right, 4)],
+        ],
         _ => panic!("unexpected grid order"),
     };
     result
 }
 
-fn parse_grid(input: &str, order: &Vec<Vec<Option<(Facing,usize)>>>, size: usize) -> Vec<Vec<Vec<GridLocation>>> {
-
+fn parse_grid(
+    input: &str,
+    order: &Vec<Vec<Option<(Facing, usize)>>>,
+    size: usize,
+) -> Vec<Vec<Vec<GridLocation>>> {
     // parse the grid in it's original shape
-    let mut big_grid: Vec<Vec<Vec<GridLocation>>> = vec![Vec::new();7];
+    let mut big_grid: Vec<Vec<Vec<GridLocation>>> = vec![Vec::new(); 7];
     let mut row = 0;
     for line in input.split('\n') {
         let chars = line
-        .as_bytes()
-        .into_iter()
-        .enumerate()
-        .map(|(idx, byte)| {
+            .as_bytes()
+            .into_iter()
+            .enumerate()
+            .map(|(idx, byte)| {
                 let val = match byte {
                     b' ' => GridVal::Void,
                     b'.' => GridVal::Space,
                     b'#' => GridVal::Rock,
-                    _ => panic!("found bad grid val")
+                    _ => panic!("found bad grid val"),
                 };
-                GridLocation{val, orig_row: row, orig_cal: idx as u32, }
-            }
-        ).collect();
+                GridLocation {
+                    val,
+                    orig_row: row,
+                    orig_col: idx as u32,
+                }
+            })
+            .collect();
 
         big_grid[0].push(chars);
         row += 1;
@@ -95,22 +113,22 @@ fn parse_grid(input: &str, order: &Vec<Vec<Option<(Facing,usize)>>>, size: usize
     for row in 0..order.len() {
         for g in 0..order[row].len() {
             let row_start = row * size as usize;
-            let col_start = g*size as usize;
+            let col_start = g * size as usize;
             let mut face_row = 0;
             match order[row][g] {
-                Some((Top,side)) => {
-                    for input_row in row_start..(row_start+size as usize) {
+                Some((Top, side)) => {
+                    for input_row in row_start..(row_start + size as usize) {
                         big_grid[side].insert(face_row, Vec::new());
-                        for input_col in col_start..(col_start+ size as usize) {
+                        for input_col in col_start..(col_start + size as usize) {
                             let grid_val = big_grid[0][input_row][input_col].clone();
                             big_grid[side][face_row].push(grid_val);
                         }
-                        face_row +=1;
+                        face_row += 1;
                     }
-                },
-                Some((Left,side)) => todo!(),
-                Some((Right,side)) => todo!(),
-                Some((Bottom,side)) => todo!(),
+                }
+                Some((Left, side)) => todo!(),
+                Some((Right, side)) => todo!(),
+                Some((Bottom, side)) => todo!(),
                 None => (),
             };
         }
@@ -118,23 +136,22 @@ fn parse_grid(input: &str, order: &Vec<Vec<Option<(Facing,usize)>>>, size: usize
     big_grid
 }
 
-fn parse_order(input: &str) -> Vec<Vec<Option<(Facing,usize)>>> {
-    let mut output: Vec<Vec<Option<(Facing,usize)>>> = Vec::new();
+fn parse_order(input: &str) -> Vec<Vec<Option<(Facing, usize)>>> {
+    let mut output: Vec<Vec<Option<(Facing, usize)>>> = Vec::new();
     for line in input.lines() {
         let orientations = line
             .split_whitespace()
             .map(|pair| (pair.as_bytes()[0], pair.as_bytes()[1]))
-            .map(|(side, orientation)| {
-                match (side,orientation,PHASE) {
-                    (b'B',_,_) => None,
-                    (side, _, 1) => Some((Top, (side - b'0') as usize)),
-                    (side, b'T', 2) => Some((Top, (side - b'0') as usize)),
-                    (side, b'R', 2) => Some((Right,(side - b'0') as usize)),
-                    (side, b'L', 2) => Some((Left,(side - b'0') as usize)),
-                    (side, b'B', 2) => Some((Bottom,(side - b'0') as usize)),
-                    _ => panic!("unexpected order"),
-                }
-            }).collect();
+            .map(|(side, orientation)| match (side, orientation, PHASE) {
+                (b'B', _, _) => None,
+                (side, _, 1) => Some((Top, (side - b'0') as usize)),
+                (side, b'T', 2) => Some((Top, (side - b'0') as usize)),
+                (side, b'R', 2) => Some((Right, (side - b'0') as usize)),
+                (side, b'L', 2) => Some((Left, (side - b'0') as usize)),
+                (side, b'B', 2) => Some((Bottom, (side - b'0') as usize)),
+                _ => panic!("unexpected order"),
+            })
+            .collect();
         output.push(orientations);
     }
     output
@@ -149,7 +166,7 @@ enum Direction {
 use Direction::*;
 
 fn parse_directions(input: &str) -> Vec<Direction> {
-    let mut directions= Vec::new();
+    let mut directions = Vec::new();
     let dir_bytes = input.as_bytes();
     let length = dir_bytes.len();
     let mut cursor = 0;
@@ -159,13 +176,13 @@ fn parse_directions(input: &str) -> Vec<Direction> {
             b'0'..=b'9' => {
                 let mut count = char as usize - b'0' as usize;
                 loop {
-                    if cursor == length -1 {
+                    if cursor == length - 1 {
                         break;
                     }
-                    let next = dir_bytes[cursor+1];
+                    let next = dir_bytes[cursor + 1];
                     if next >= b'0' && next <= b'9' {
                         count = (count * 10) + (next as usize - b'0' as usize);
-                        cursor = cursor +1;
+                        cursor = cursor + 1;
                     } else {
                         break;
                     }
@@ -174,10 +191,10 @@ fn parse_directions(input: &str) -> Vec<Direction> {
             }
             b'L' => TurnLeft,
             b'R' => TurnRight,
-            _ => panic!("unexpected direction")
+            _ => panic!("unexpected direction"),
         };
         directions.push(dir);
-        cursor = cursor +1;
+        cursor = cursor + 1;
     }
     directions
 }
@@ -214,77 +231,148 @@ impl Location {
     }
 }
 
-fn rotate_transform(leaving_side: Facing, arriving_side: (Facing, usize), old_x: usize, old_y: usize, face_size: usize) -> Location {
-    todo!();
+fn rotate_transform(
+    leaving_side: Facing,
+    arriving_side: (Facing, usize),
+    old_x: usize,
+    old_y: usize,
+    face_size: usize,
+) -> Location {
+    match (leaving_side,arriving_side.0) {
+        (Bottom, Top) => Location{x:old_x, y: 0, grid: arriving_side.1, facing: Bottom},
+        (Left, Right) => Location{x:face_size-1, y:old_y, grid: arriving_side.1, facing: Left},
+        (Right, Left) => Location{x:0, y:old_y, grid: arriving_side.1, facing: Right},
+        (Top, Bottom) => Location{x:old_x, y:face_size-1, grid: arriving_side.1, facing: Top},
+        (Top, Top) => todo!(),
+        (Top, Left) => todo!(),
+        (Top, Right) => todo!(),
+        (Left, Top) => todo!(),
+        (Left, Left) => todo!(),
+        (Left, Bottom) => todo!(),
+        (Right, Top) => todo!(),
+        (Right, Right) => todo!(),
+        (Right, Bottom) => todo!(),
+        (Bottom, Left) => todo!(),
+        (Bottom, Right) => todo!(),
+        (Bottom, Bottom) => todo!(),
+        
+    }
 }
 
-fn try_move( cur_location: &Location, delta: &(i32,i32), face_size: usize, orientation: &Vec<Vec<(Facing, usize)>>) -> Location {
-
+fn try_move(
+    cur_location: &Location,
+    delta: &(i32, i32),
+    face_size: usize,
+    orientation: &Vec<Vec<(Facing, usize)>>,
+) -> Location {
     let new_location: Location = if delta.0 == -1 && cur_location.x == 0 {
         // off the left
-        rotate_transform(Left, orientation[cur_location.grid as usize][1], cur_location.x, cur_location.y, face_size)
-    } else if delta.0 == 1 && cur_location.x == face_size{
+        rotate_transform(
+            Left,
+            orientation[cur_location.grid as usize][1],
+            cur_location.x,
+            cur_location.y,
+            face_size,
+        )
+    } else if delta.0 == 1 && cur_location.x == face_size-1 {
         // off the right
-        rotate_transform(Right, orientation[cur_location.grid as usize][2], cur_location.x, cur_location.y, face_size)
+        rotate_transform(
+            Right,
+            orientation[cur_location.grid as usize][2],
+            cur_location.x,
+            cur_location.y,
+            face_size,
+        )
     } else if delta.1 == -1 && cur_location.y == 0 {
         // off the top
-        rotate_transform(Top, orientation[cur_location.grid as usize][0], cur_location.x, cur_location.y, face_size)
-    } else if delta.1 == 1 && cur_location.y == face_size {
+        rotate_transform(
+            Top,
+            orientation[cur_location.grid as usize][0],
+            cur_location.x,
+            cur_location.y,
+            face_size,
+        )
+    } else if delta.1 == 1 && cur_location.y == face_size-1 {
         // off the bottom
-        rotate_transform(Bottom, orientation[cur_location.grid as usize][3], cur_location.x, cur_location.y, face_size)
-    } else { 
+        rotate_transform(
+            Bottom,
+            orientation[cur_location.grid as usize][3],
+            cur_location.x,
+            cur_location.y,
+            face_size,
+        )
+    } else {
         // internal to a face
-        Location{x:(cur_location.x as i32 + delta.0) as usize, y: (cur_location.y as i32 + delta.1) as usize, grid: cur_location.grid, facing: cur_location.facing}
+        Location {
+            x: (cur_location.x as i32 + delta.0) as usize,
+            y: (cur_location.y as i32 + delta.1) as usize,
+            grid: cur_location.grid,
+            facing: cur_location.facing,
+        }
     };
     new_location
 }
 
-fn perform_walk(orientation: &Vec<Vec<(Facing, usize)>>, grid: &Vec<Vec<Vec<GridLocation>>>, directions: &Vec<Direction>, location: &Location, face_size :usize) -> u32 {
+fn perform_walk(
+    orientation: &Vec<Vec<(Facing, usize)>>,
+    grid: &Vec<Vec<Vec<GridLocation>>>,
+    directions: &Vec<Direction>,
+    location: &Location,
+    face_size: usize,
+) -> u32 {
     let mut my_location = location.clone();
 
     for d in directions {
         match d {
             Forward(count) => {
-                let delta:(i32,i32) = match my_location.facing {
-                    Facing::Top => (0,-1),
-                    Facing::Bottom => (0,1),
-                    Facing::Left => (-1,0),
-                    Facing::Right => (1,0),
+                let delta: (i32, i32) = match my_location.facing {
+                    Facing::Top => (0, -1),
+                    Facing::Bottom => (0, 1),
+                    Facing::Left => (-1, 0),
+                    Facing::Right => (1, 0),
                 };
 
-                for _ in 0..*count {
-                    let next_location:Location = try_move(&my_location, &delta, face_size, orientation);
+                for _walk_cnt in 0..*count {
+                    let next_location: Location =
+                        try_move(&my_location, &delta, face_size, orientation);
 
-                    if grid[next_location.grid as usize][next_location.x][next_location.y].val == GridVal::Space {
+                    if grid[next_location.grid as usize][next_location.y][next_location.x].val
+                        == GridVal::Space
+                    {
+                        //println!("{:?}", my_location.facing);
                         my_location = next_location;
                     } else {
+                        //println!("bump");
                         // collision
                         break;
                     }
                 }
-
-            },
+            }
             _ => my_location.turn(d),
         }
     }
 
     let final_grid_location = grid[my_location.grid as usize][my_location.y][my_location.x].clone();
-    let final_score = ((final_grid_location.orig_row+1) * 1000) 
-                                + ((final_grid_location.orig_cal+1) * 8) 
-                                + my_location.score();
+    let final_score = ((final_grid_location.orig_row + 1) * 1000)
+        + ((final_grid_location.orig_col + 1) * 4)
+        + my_location.score();
     final_score
 }
 fn main() {
-    let input : Input = if IS_SAMPLE {
-        Input{  grid: include_str!("../sample-grid.txt"),
-                dir: include_str!("../sample-directions.txt"),
-                order:include_str!("../sample-order.txt"),
-                size: 4}
+    let input: Input = if IS_SAMPLE {
+        Input {
+            grid: include_str!("../sample-grid.txt"),
+            dir: include_str!("../sample-directions.txt"),
+            order: include_str!("../sample-order.txt"),
+            size: 4,
+        }
     } else {
-        Input{  grid: include_str!("../day22-grid.txt"),
-                dir: include_str!("../day22-directions.txt"),
-                order: include_str!("../day22-order.txt"),
-                size: 50}
+        Input {
+            grid: include_str!("../day22-grid.txt"),
+            dir: include_str!("../day22-directions.txt"),
+            order: include_str!("../day22-order.txt"),
+            size: 50,
+        }
     };
 
     let input_sections = parse_order(input.order);
@@ -292,13 +380,20 @@ fn main() {
     let grid = parse_grid(input.grid, &input_sections, input.size);
     let directions = parse_directions(input.dir);
 
-    let start_location = if IS_SAMPLE {
-        Location{x:8,y:0,grid:0,facing:Facing::Right}
-    } else {
-        Location{x:50,y:0,grid:1,facing:Facing::Right}
-    };
+    let start_location =
+        Location {
+            x: 0,
+            y: 0,
+            grid: 1,
+            facing: Facing::Right,
+        };
 
-    let end_location_score = perform_walk(&grid_orientation, &grid, &directions, &start_location, input.size);
+    let end_location_score = perform_walk(
+        &grid_orientation,
+        &grid,
+        &directions,
+        &start_location,
+        input.size,
+    );
     println!("score: {end_location_score}");
-
 }
