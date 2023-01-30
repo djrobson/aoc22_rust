1ae240e76a3b161958f122b873fb57907ede8e0b
Dale Robson <drobson@gmail.com>
2023-01-29 22:19:53 -0500

    day25 all done, 2022 all done

--- day25/src/main.rs
+++ day25/src/main.rs
@@ -17,8 +17,35 @@ fn parse_input(input: &str) -> Vec<Vec<isize>> {
     result
 }
 
-fn convert_to_snafu(input: isize) -> String {
-    
+fn decimal_to_snafu(decimal: isize) -> String {
+    let mut nums: Vec<i8> = Vec::new();
+
+    let mut num = decimal;
+    let mut borrow = 0;
+    while num != 0 {
+        let mut remainder = num%5;
+        remainder +=  borrow;
+        num = num /5;
+        if remainder >= 3 {
+            remainder -=5;
+            borrow = 1;
+        } else {
+            borrow = 0;
+        }
+        nums.push(remainder as i8);
+    }
+
+    let result: String = nums.iter().rev().map(|n| {
+        match n {
+            -2 => '=',
+            -1 => '-',
+            0 => '0',
+            1 => '1',
+            2 => '2',
+            _ => panic!("unexpected conversion"),
+        }
+    }).collect();
+    result
 }
 
 fn main() {
@@ -29,11 +56,12 @@ fn main() {
     };
     let inputs =  parse_input(INPUT);
     let fuel_requirements: Vec<isize> = inputs.iter().map(|line| {
-        line.iter().rev().enumerate().map(|(position, quintits)| {
+        line.iter().rev().enumerate().map(|(position, quint)| {
             let pos = 5_isize.pow(position as u32);
-            pos as isize * *quintits as isize
+            pos as isize * *quint as isize
         }).sum::<isize>()
     }).collect();
     let total:isize = fuel_requirements.iter().sum();
     println!("{total}");
+    println!("{}", decimal_to_snafu(total));
 }
